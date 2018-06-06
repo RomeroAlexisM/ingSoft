ca215f75b9ffcf87e30fbdb69e06cf98dceb4c92
piquiferrato <juanferrato1994@gmail.com>
2018-06-06 19:52:21 -0300

    creacion de entorno virtual

--- servidor/proyecto/controlpedidos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ControlpedidosConfig(AppConfig):
-    name = 'controlpedidos'
--- servidor/proyecto/controlpedidos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('prueba', views.prueba, name='index'),
-    path('tecnico/pedidos', views.tecnico_pedidos, name='lista_pedidos_tecnico'),
-]
