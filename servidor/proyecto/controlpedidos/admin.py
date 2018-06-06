3d1fbf073fe9e6551987258c5e6dadeb1fe45b59
piquiferrato <juanferrato1994@gmail.com>
2018-06-05 21:38:44 -0300

    CRUD de superadmin funcionando

--- servidor/proyecto/controlpedidos/admin.py
+++ servidor/proyecto/controlpedidos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tecnico, Pedido, Usuario
+
+admin.site.register(Pedido)
+admin.site.register(Usuario)
+admin.site.register(Tecnico)
