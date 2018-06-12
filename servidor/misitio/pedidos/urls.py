a7561094e238fc0515ad2587086e7ec4bce39bb9
piquiferrato <juanferrato1994@gmail.com>
2018-06-12 13:49:36 -0300

    pequeña refactorización

--- servidor/misitio/pedidos/urls.py
+++ servidor/misitio/pedidos/urls.py
@@ -8,9 +8,6 @@ router = routers.DefaultRouter()
 router.register('usuarios', views.UsuarioView)
 router.register('tecnicos', views.TecnicoView)
 router.register('pedidos', views.PedidoView)
-router.register('usuarios/abm/<id>', views.UsuarioABMView)
-router.register('tecnicos/abm/<id>', views.Tecnico)
-router.register('pedidos/abm/<id>', views.)
 
 
 urlpatterns = [
--- servidor/misitio/pedidos/views.py
+++ servidor/misitio/pedidos/views.py
@@ -6,31 +6,18 @@ from .serializers import *
 # Create your views here.
 
 class UsuarioView(viewsets.ModelViewSet):
+    lookup_field = 'id'
     serializer_class = UsuarioSerializer
     queryset = Usuario.objects.all()
 
 
 class TecnicoView(viewsets.ModelViewSet):
+    lookup_field = 'id'
     serializer_class = TecnicoSerializer
     queryset = Tecnico.objects.all()
 
 
 class PedidoView(viewsets.ModelViewSet):
-    serializer_class = PedidoSerializer
-    queryset = Pedido.objects.all()
-
-
-class UsuarioABMView(viewsets.ModelViewSet):
-    lookup_field = 'id'
-    serializer_class = UsuarioSerializer
-    queryset = Usuario.objects.all()
-
-class TecnicoABMView(viewsets.ModelViewSet):
-    lookup_field = 'id'
-    serializer_class = TecnicoSerializer
-    queryset = Tecnico.objects.all()
-
-class PedidoABMView(viewsets.ModelViewSet):
     lookup_field = 'id'
     serializer_class = PedidoSerializer
     queryset = Pedido.objects.all()
